1a54e480671e3f01a82a43d107bfc46c21472f86
avhirup <avhirup84jazz@gmail.com>
2018-04-13 16:38:55 +0530

    LSGAN

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import torch
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+from torch.autograd import Variable as V
 import torch.optim as optim
 import argparse
 from model import Generator, Discriminator
 from tensorboardX import SummaryWriter
-
+import tqdm
+from utils import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("type", default="DCGAN",help="Options to choose from DCGAN, WGAN, WGAN+GP, LSGAN")
-parser.add_argument("image_size",default=64, help="Image size")
-parser.add_argument("is_cuda",default=False, help="Is graphic card present?")
-parser.add_argument("batch_size",default=64, help="batch_size")
-parser.add_argument("epochs",default=2, help="Number of epochs to Train")
+parser.add_argument("--type", default="DCGAN",help="Options to choose from DCGAN, WGAN, WGAN+GP, LSGAN")
+parser.add_argument("--image_size",default=64, help="Image size")
+parser.add_argument("--is_cuda",default=False, help="Is graphic card present?")
+parser.add_argument("--batch_size",default=64, help="batch_size")
+parser.add_argument("--epochs",default=2, help="Number of epochs to Train")
+parser.add_argument("--lr",default=1e-3, help="learning_rate")
 
 args = parser.parse_args()
-print(args.type)
+print(args)
 
-args={'batch_size':64,'image_size':img_size,"is_cuda":False,"NUM_OF_EPOCHS":2,"lr":1e-3}
 
 writer = SummaryWriter()
-batch_size=64#args.batch_size
-img_size = 64#args.image_size
-is_cuda=False#args.is_cuda
-NUM_OF_EPOCHS=2#args.epochs
-lr=1e-3#args.lr
+batch_size=args.batch_size
+img_size = args.image_size
+is_cuda=args.is_cuda
+NUM_OF_EPOCHS=args.epochs
+lr=args.lr
+# args={'batch_size':64,'image_size':img_size,"is_cuda":False,"NUM_OF_EPOCHS":2,"lr":1e-3}
 #Load Data
 transform = transforms.Compose([
 		transforms.Scale(img_size),
@@ -50,6 +53,8 @@ G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
 D_optimizer = optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
 
 for epoch in range(NUM_OF_EPOCHS):
+	ind=0
+	l=len(train_loader)
 	for batch,labels in tqdm.tqdm(train_loader):
 		z_ = torch.randn((batch_size, 100)).view(-1, 100, 1, 1)
 		#train_real
@@ -57,15 +62,15 @@ for epoch in range(NUM_OF_EPOCHS):
 			real_batch,noise=V(batch.cuda()),V(z_.cuda())
 		else:
 			real_batch,noise=V(batch),V(z_)	
-
 		data={}
 		data['real_batch']=real_batch
 		data['noise']=noise	
-		args['epoch']=epoch
+		args['epoch']=epoch*l+ind
+		ind=ind+1
 		train_discriminator(D,G,data,D_optimizer,args,writer)
 		train_generator(D,G,data,G_optimizer,args,writer)
-
-
+	if epoch%10==0:
+		validate(G,writer,epoch)	
 
 
 
--- utils.py
+++ utils.py
@@ -1,54 +1,136 @@
 import torch.nn as nn
 import tqdm
 from torch.autograd import Variable as V
-
+import torch.nn.functional as F
+import torch 
 def reset_grad(M):
 	if not isinstance(M,list):
 		M=[M]
 	for m in M:	
 		m.zero_grad()
 
-def train_DCGAN_discriminator(D,G,train_loader,D_optimizer):
-	y_real_ = torch.ones(batch_size)
-	y_fake_ = torch.zeros(batch_size)
+def train_DCGAN_discriminator(D,G,data,D_optimizer,args,writer):
+	y_real_ = V(torch.ones(args['batch_size']))
+	y_fake_ = V(torch.zeros(args['batch_size']))
 	BCE_loss=nn.BCELoss()
-	for batch,labels in tqdm.tqdm(train_loader):
-		z_ = torch.randn((batch_size, 100)).view(-1, 100, 1, 1)
-		#train_real
-		if is_cuda:
-			real_batch,y_real_,y_fake_,noise=V(batch.cuda()),V(y_real_.cuda()),V(y_fake_.cuda()),V(z_.cuda())
-		else:
-			real_batch,y_real_,y_fake_,noise=V(batch),V(y_real_),V(y_fake_),V(z_)
-		D_real_loss=BCE_loss(D(real_batch),y_real_)
-		G_result=G(noise)
-		D_fake_loss=BCE_loss(D(G_result),y_fake_)
+	if args['is_cuda']:
+		y_real_,y_fake_=y_real_.cuda(),y_fake_.cuda()
+	D_real_loss=BCE_loss(F.sigmoid(D(data['real_batch']).squeeze()),y_real_)
+	G_result=G(data['noise'])
+	D_fake_loss=BCE_loss(F.sigmoid(D(G_result).squeeze()),y_fake_)
 
-		D_loss=D_real_loss+D_fake_loss
+	D_loss=D_real_loss+D_fake_loss
+	D_loss.backward()
+	D_optimizer.step()
+	reset_grad([D,G])
+	writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+	writer.export_scalars_to_json("./all_scalars.json")
+
+def train_DCGAN_generator(D,G,data,G_optimizer,args,writer):
+	BCE_loss=nn.BCELoss()
+	y_real_ = V(torch.ones(args['batch_size']))
+	if args['is_cuda']:
+		y_real_=y_real_.cuda()
+	G_result=G(data['noise'])
+	G_loss=BCE_loss(F.sigmoid(D(G_result).squeeze()),y_real_)
+	G_loss.backward()
+	G_optimizer.step()
+	reset_grad([D,G])
+	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.export_scalars_to_json("./all_scalars.json")
+
+
+
+def train_WGAN_discriminator(D,G,data,D_optimizer,args,writer):
+	for critic_repetitions in range(args['n_critic']):
+		real_embed=D(data['real_batch'])
+		G_result=G(data['noise'])
+		fake_embed=D(G_result)
+		Penalty=V(torch.zeros(1))
+
+		if args['is_GP']:
+			alpha=random.random()
+			x_hat = (alpha*G_result+(1-alpha)*V(img_r)).detach()
+			x_hat.requires_grad = True
+			loss_D = D(x_hat).sum()
+			loss_D.backward()
+			x_hat.grad.volatile = False			
+			Penalty=((x_hat.grad -1)**2 * args['LAMBDA']).mean()
+
+		D_loss=(real_embed-fake_embed).mean()+Penalty
 		D_loss.backward()
 		D_optimizer.step()
 		reset_grad([D,G])
-		print (1)
-		break
+		if not args['is_GP']:
+			for p in D.parameters():
+				p.data.clamp_(args["clamp_lower"],args["clamp_upper"])
 
-def train_DCGAN_generator(D,G,train_loader):
-	y_real_ = torch.ones(batch_size)
-	BCE_loss=nn.BCELoss()
-	for batch,labels in tqdm(train_loader):
-		z_ = torch.randn((batch_size, 100)).view(-1, 100, 1, 1)
-		#train_real
-		if is_cuda:
-			real_batch,y_real_,y_fake_,noise=V(batch.cuda()),V(y_real_.cuda()),V(y_fake_.cuda()),V(z_.cuda())
-		else:
-			real_batch,y_real_,y_fake_,noise=V(batch),V(y_real_),V(y_fake_),V(z_)		
-		G_result=G(noise)
-		G_loss=BCE_loss(D(G_result),y_real_)
-		G_loss.backward()
-		G_optimizer.step()
-		reset_grad([D,G])
+		writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+		writer.export_scalars_to_json("./all_scalars.json")
 
+
+def train_WGAN_generator(D,G,data,G_optimizer,args,writer):
+	G_result=G(data['noise'])
+	fake_embed=D(G_result)
+	G_loss=-fake_embed.mean()
+	G_loss.backward()
+	G_optimizer.step()
+	reset_grad([D,G])
+	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.export_scalars_to_json("./all_scalars.json")
 
 
 
+def train_LSGAN_discriminator(D,G,data,D_optimizer,args,writer):
+	real_embed=D(data['real_batch'])
+	G_result=G(data['noise'])
+	fake_embed=D(G_result)
 
-def train_discriminator(D,G,train_loader,type="DCGAN"):
-	#train on 
+	D_loss=args['LAMBDA']*((real_embed-1)**2).mean()+((fake_embed)**2).mean()
+	D_loss.backward()
+	D_optimizer.step()
+	reset_grad([D,G])
+	writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+	writer.export_scalars_to_json("./all_scalars.json")	
+
+
+
+def train_LSGAN_generator(D,G,data,G_optimizer,args,writer):
+	G_result=G(data['noise'])
+	fake_embed=D(G_result)
+	G_loss=args['LAMBDA']*args['LAMBDA']*((fake_embed-1)**2).mean()
+	G_loss.backward()
+	G_optimizer.step()
+	reset_grad([D,G])
+	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.export_scalars_to_json("./all_scalars.json")
+
+
+def train_discriminator(D,G,data,D_optimizer,args,writer,type="DCGAN"):
+	if type=='DCGAN':
+		train_DCGAN_discriminator(D,G,data,D_optimizer,args,writer)
+	elif type=='WGAN':
+		train_WGAN_discriminator(D,G,data,D_optimizer,args,writer)
+	elif type=='LSGAN':
+		train_LSGAN_discriminator(D,G,data,D_optimizer,args,writer)
+	else:
+		print("Implementation Remaining")
+		pass
+
+
+def train_generator(D,G,data,G_optimizer,args,writer,type="DCGAN"):
+	if type=='DCGAN':
+		train_DCGAN_generator(D,G,data,G_optimizer,args,writer)
+	elif type=='WGAN':
+		train_WGAN_generator(D,G,data,G_optimizer,args,writer)		
+	elif type=='LSGAN':
+		train_LSGAN_generator(D,G,data,G_optimizer,args,writer)		
+	else:
+		print("Implementation Remaining")
+		pass
+
+def validate(G,writer,epoch,type='DCGAN'):
+	z_ = torch.randn((5, 100)).view(-1, 100, 1, 1)
+	G_result=G(V(z_))
+	writer.add_image("GeneratedImage/"+type,G_result,epoch)
+
