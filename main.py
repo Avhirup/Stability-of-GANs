0489548227a2ad337728cf743c9d65c27ab3df54
avhirup <avhirup84jazz@gmail.com>
2018-04-13 19:41:43 +0530

    LSGAN

--- main.py
+++ main.py
@@ -10,25 +10,28 @@ import tqdm
 from utils import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--type", default="DCGAN",help="Options to choose from DCGAN, WGAN, WGAN+GP, LSGAN")
-parser.add_argument("--image_size",default=64, help="Image size")
-parser.add_argument("--is_cuda",default=False, help="Is graphic card present?")
-parser.add_argument("--batch_size",default=64, help="batch_size")
-parser.add_argument("--epochs",default=2, help="Number of epochs to Train")
-parser.add_argument("--lr",default=1e-3, help="learning_rate")
+parser.add_argument("type", default="DCGAN",help="Options to choose from DCGAN, WGAN, WGAN+GP, LSGAN",type=str)
+parser.add_argument("-image_size",default=64, help="Image size",type=int)
+parser.add_argument("-is_cuda",default=False, help="Is graphic card present?",type=bool)
+parser.add_argument("-batch_size",default=64, help="batch_size",type=int)
+parser.add_argument("epochs",default=2, help="Number of epochs to Train",type=int)
+parser.add_argument("-lr",default=1e-4, help="learning_rate",type=float)
+parser.add_argument("-LAMBDA",default=1e-3, help="lambda",type=float)
+parser.add_argument("-is_GP",default=False, help="To use Gradient Penalty",type=bool)
+parser.add_argument("-n_critic",default=3, help="number of critic iterations per Generator iterations",type=int)
+parser.add_argument("-clamp_upper",default=1e-2, help="weight clipping upper limit",type=float)
+parser.add_argument("-clamp_lower",default=-1e-2, help="weight clipping lower limit",type=float)
 
 args = parser.parse_args()
-print(args)
-
-
 writer = SummaryWriter()
 batch_size=args.batch_size
 img_size = args.image_size
 is_cuda=args.is_cuda
 NUM_OF_EPOCHS=args.epochs
 lr=args.lr
-# args={'batch_size':64,'image_size':img_size,"is_cuda":False,"NUM_OF_EPOCHS":2,"lr":1e-3}
+print (args.type,batch_size,img_size,is_cuda,NUM_OF_EPOCHS,lr)
 #Load Data
+print ("--------------------------------",img_size)
 transform = transforms.Compose([
 		transforms.Scale(img_size),
 		transforms.ToTensor(),
@@ -49,8 +52,8 @@ else:
 G.weight_init(0,0.02)
 D.weight_init(0,0.02)
 
-G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
-D_optimizer = optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
+G_optimizer = optim.Adam(G.parameters(), lr=1e-6)
+D_optimizer = optim.Adam(D.parameters(), lr=lr)
 
 for epoch in range(NUM_OF_EPOCHS):
 	ind=0
@@ -65,7 +68,7 @@ for epoch in range(NUM_OF_EPOCHS):
 		data={}
 		data['real_batch']=real_batch
 		data['noise']=noise	
-		args['epoch']=epoch*l+ind
+		args.epoch=epoch*l+ind
 		ind=ind+1
 		train_discriminator(D,G,data,D_optimizer,args,writer)
 		train_generator(D,G,data,G_optimizer,args,writer)
--- utils.py
+++ utils.py
@@ -10,10 +10,10 @@ def reset_grad(M):
 		m.zero_grad()
 
 def train_DCGAN_discriminator(D,G,data,D_optimizer,args,writer):
-	y_real_ = V(torch.ones(args['batch_size']))
-	y_fake_ = V(torch.zeros(args['batch_size']))
+	y_real_ = V(torch.ones(args.batch_size))
+	y_fake_ = V(torch.zeros(args.batch_size))
 	BCE_loss=nn.BCELoss()
-	if args['is_cuda']:
+	if args.is_cuda:
 		y_real_,y_fake_=y_real_.cuda(),y_fake_.cuda()
 	D_real_loss=BCE_loss(F.sigmoid(D(data['real_batch']).squeeze()),y_real_)
 	G_result=G(data['noise'])
@@ -23,49 +23,49 @@ def train_DCGAN_discriminator(D,G,data,D_optimizer,args,writer):
 	D_loss.backward()
 	D_optimizer.step()
 	reset_grad([D,G])
-	writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+	writer.add_scalar("Discriminator/Error",D_loss,args.epoch)
 	writer.export_scalars_to_json("./all_scalars.json")
 
 def train_DCGAN_generator(D,G,data,G_optimizer,args,writer):
 	BCE_loss=nn.BCELoss()
-	y_real_ = V(torch.ones(args['batch_size']))
-	if args['is_cuda']:
+	y_real_ = V(torch.ones(args.batch_size))
+	if args.is_cuda:
 		y_real_=y_real_.cuda()
 	G_result=G(data['noise'])
 	G_loss=BCE_loss(F.sigmoid(D(G_result).squeeze()),y_real_)
 	G_loss.backward()
 	G_optimizer.step()
 	reset_grad([D,G])
-	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.add_scalar("Generator/Error",G_loss,args.epoch)
 	writer.export_scalars_to_json("./all_scalars.json")
 
 
 
 def train_WGAN_discriminator(D,G,data,D_optimizer,args,writer):
-	for critic_repetitions in range(args['n_critic']):
+	for critic_repetitions in range(args.n_critic):
 		real_embed=D(data['real_batch'])
 		G_result=G(data['noise'])
 		fake_embed=D(G_result)
 		Penalty=V(torch.zeros(1))
 
-		if args['is_GP']:
+		if args.is_GP:
 			alpha=random.random()
-			x_hat = (alpha*G_result+(1-alpha)*V(img_r)).detach()
+			x_hat = (alpha*G_result+(1-alpha)*V(data['real_batch'])).detach()
 			x_hat.requires_grad = True
 			loss_D = D(x_hat).sum()
 			loss_D.backward()
 			x_hat.grad.volatile = False			
-			Penalty=((x_hat.grad -1)**2 * args['LAMBDA']).mean()
+			Penalty=((x_hat.grad -1)**2 * args.LAMBDA).mean()
 
 		D_loss=(real_embed-fake_embed).mean()+Penalty
 		D_loss.backward()
 		D_optimizer.step()
 		reset_grad([D,G])
-		if not args['is_GP']:
+		if not args.is_GP:
 			for p in D.parameters():
-				p.data.clamp_(args["clamp_lower"],args["clamp_upper"])
+				p.data.clamp_(args.clamp_lower,args.clamp_upper)
 
-		writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+		writer.add_scalar("Discriminator/Error",D_loss,args.epoch)
 		writer.export_scalars_to_json("./all_scalars.json")
 
 
@@ -76,7 +76,7 @@ def train_WGAN_generator(D,G,data,G_optimizer,args,writer):
 	G_loss.backward()
 	G_optimizer.step()
 	reset_grad([D,G])
-	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.add_scalar("Generator/Error",G_loss,args.epoch)
 	writer.export_scalars_to_json("./all_scalars.json")
 
 
@@ -86,11 +86,11 @@ def train_LSGAN_discriminator(D,G,data,D_optimizer,args,writer):
 	G_result=G(data['noise'])
 	fake_embed=D(G_result)
 
-	D_loss=args['LAMBDA']*((real_embed-1)**2).mean()+((fake_embed)**2).mean()
+	D_loss=args.LAMBDA*((real_embed-1)**2).mean()+((fake_embed)**2).mean()
 	D_loss.backward()
 	D_optimizer.step()
 	reset_grad([D,G])
-	writer.add_scalar("Discriminator/Error",D_loss,args['epoch'])
+	writer.add_scalar("Discriminator/Error",D_loss,args.epoch)
 	writer.export_scalars_to_json("./all_scalars.json")	
 
 
@@ -98,11 +98,11 @@ def train_LSGAN_discriminator(D,G,data,D_optimizer,args,writer):
 def train_LSGAN_generator(D,G,data,G_optimizer,args,writer):
 	G_result=G(data['noise'])
 	fake_embed=D(G_result)
-	G_loss=args['LAMBDA']*args['LAMBDA']*((fake_embed-1)**2).mean()
+	G_loss=args.LAMBDA*args.LAMBDA*((fake_embed-1)**2).mean()
 	G_loss.backward()
 	G_optimizer.step()
 	reset_grad([D,G])
-	writer.add_scalar("Generator/Error",G_loss,args['epoch'])
+	writer.add_scalar("Generator/Error",G_loss,args.epoch)
 	writer.export_scalars_to_json("./all_scalars.json")
 
 
